4617bfa62ee2fba006b92ae6264626309ea32462
Matthew DeVenny <matthew.devenny@gmail.com>
2026-05-27 15:15:29 -0700

    Fix checksuite-id (#10)
    Signed-off-by: Matthew DeVenny <matt@codecargo.com>

--- src/GitHub/Models/CheckRun_check_suite.cs
+++ src/GitHub/Models/CheckRun_check_suite.cs
@@ -15,7 +15,7 @@ namespace GitHub.Models
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
         /// <summary>The id property</summary>
-        public int? Id { get; set; }
+        public long? Id { get; set; }
         /// <summary>
         /// Instantiates a new <see cref="global::GitHub.Models.CheckRun_check_suite"/> and sets the default values.
         /// </summary>
@@ -41,7 +41,7 @@ namespace GitHub.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
-                { "id", n => { Id = n.GetIntValue(); } },
+                { "id", n => { Id = n.GetLongValue(); } },
             };
         }
         /// <summary>
@@ -51,7 +51,7 @@ namespace GitHub.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
-            writer.WriteIntValue("id", Id);
+            writer.WriteLongValue("id", Id);
             writer.WriteAdditionalData(AdditionalData);
         }
     }
